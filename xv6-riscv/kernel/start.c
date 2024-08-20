721c893c20a1e038a10111a08fc4f480772140b8
landonbr <landonbrown0797@gmail.com>
2024-08-20 17:56:17 -0400

    Making the uart present a more useful message
    Hello World. This also marks the transition into
    C which we will go from here on out. I am still deciding
    on if I want to inline a bunch of useful asm function
    in C such as reading certain status registers (i.e
    mhartid in start.c). I may try to just start transitioning
    into C now and in the future there could be a big revamp
    into the register functions and where they are declared.

--- xv6-riscv/kernel/start.c
+++ xv6-riscv/kernel/start.c
@@ -1,5 +1,22 @@
+#include <stdint.h>
+
+extern uint64_t _write_uart(void *val);
+
+static inline uint64_t r_mhartid()
+{
+  uint64_t ra;
+  asm volatile("csrr %0, mhartid" : "=r" (ra) );
+  return ra;
+}
 
 void start(void)
 {
-    while(1);
+    uint64_t ra = 0;
+    ra = r_mhartid();
+
+    if (!ra) {
+        _write_uart("Hello, World\n");
+    }
+
+    while (1);
 }
